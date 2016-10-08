afab053ef2826ab383ab3e695fb0b80fd0721ea6
jesseharold <jesseharold@gmail.com>
2016-10-08 11:20:49 -0700

    changing code to make it an object - currently broken

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,155 @@
+// the game Object
+var hangmanGame = {
+	// this gets definted by the game init function
+	dictionary : [],
 
-// all possible words to guess
-var dictionary = [
-	"Jaime",
-	"Cersei",
-	"Tyrion",
-	"Blackfish",
-	"Sansa",
-	"Arya",
-	"Summer",
-	"Winter",
-	"Others",
-	"Dothraki",
-	"Valyrian",
-	"Targaryen",
-	"Direwolf",
-	"Lannister",
-	"Riverrun",
-	"Winterfell",
-	"Dreadfort",
-	"Mereen",
-	"Sunspear",
-	"Clegane",
-	"Dragonfire",
-	"Stoneheart",
-	"Hodor",
-	"Coldhands",
-	"Mormont",
-	"Longclaw"
-];
+	// what to display when a part gets hung, replace these w images later?
+	bodyParts : ["head", "torso", "right arm", "left arm", "right leg", "left leg"],
 
-// keep track of how much of the hangman is hung
+	// keep track of how much of the hangman is hung
+	hangman : [false, false, false, false, false, false],
+
+	// keep track of incorrect letters guessed
+	guessedWrongLetters : [],
+
+	// keep track of incorrect letters guessed
+	guessedRightLetters : [],
+
+	// word the user is currently trying to guess
+	currentAnswer : "",
+
+	// stop the game from registering key presses at some times
+	waitingForAGuess : true,
+
+	// METHODS
+	gameInit : function(dict){
+		this.dictionary = dict;
+		document.addEventListener('keydown', function(event) {
+				var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+		    	guessLetter(userInput.toLowerCase());
+		});
+
+		newGame();
+	},
+
+	newGame : function(){
+		var rand = Math.floor(Math.random() * dictionary.length);
+		currentAnswer = dictionary[rand].toLowerCase();
+		console.log("answer: " + currentAnswer);
+		displayBlanks();
+		waitingForAGuess = true;
+	},
+
+	displayBlanks : function(){
+		var html = "";
+		for(var i = 0; i < currentAnswer.length; i++){
+			if (guessedRightLetters[i]){
+				html += " " + guessedRightLetters[i] + " ";
+			} else {
+				html += " _ ";
+			}
+		}
+		document.querySelector('#letterBlanks').innerHTML = html;
+	},
+
+	guessLetter : function(input){
+		// don't process the keypress if game isn't in the right state
+		if (waitingForAGuess === true){
+			var position = currentAnswer.indexOf(input);
+			if (position < 0){
+				turnFail(input);
+			} else {
+				turnSuccess(position, input);
+			}
+		}
+	},
+
+	turnFail : function(letter) {
+		console.log("turnFail");
+		guessedWrongLetters.push(letter);
+		var isGameOver = false;
+		for (var i = 0; i < hangman.length; i++) {
+			if(hangman[i] === false){
+				// find the first false item in the array, make it true
+				hangman[i] = true;
+				// then exit the loop
+				i = hangman.length;
+			}
+			if (i === (hangman.length-2)){
+				// this is the last loop, which means
+				// all body parts are now completed, game is over
+					isGameOver = true; 
+			}
+		}
+		displayHangman();
+		if (isGameOver === true){
+			gameOver("lose");
+		}
+	},
+
+	turnSuccess : function(position, letter) {
+		console.log("turnSuccess");
+		// find all positions of this letter in the answer
+		// I found this solution on Stack Overflow and modified it
+	    var startIndex = 0;
+	    var index;
+	    while ((index = currentAnswer.indexOf(letter, startIndex)) > -1) {
+	        guessedRightLetters[index] = letter;
+	        startIndex = index + 1;
+	    }
+	    //console.log(guessedRightLetters);
+
+		displayBlanks();
+
+	    //test to see if the game is won
+	    var gameIsWon = true;
+	    if (guessedRightLetters.length < currentAnswer.length) {
+			gameIsWon = false;
+		} else {
+		    for (var i = 0; i < guessedRightLetters.length; i++) {
+		    	if(!guessedRightLetters[i]){
+		    		gameIsWon = false;
+		    		i = guessedRightLetters.length;
+		    	}
+		    }
+		}
+	    if (gameIsWon){
+		    gameOver("win");
+		}
+	},
+
+	displayHangman : function(){
+	//	console.log("displayHangman");
+		var html = "noose";
+		for (var i = 0; i < hangman.length; i++) {
+			if(hangman[i] === true){
+				html += "<br>Display " + bodyParts[i];
+			}
+		}
+		document.querySelector('#hangman').innerHTML = html;
+
+		// also display the wrong letters the user has already guessed
+		var html2 = "You guessed: <br>";
+		for (var j = 0; j < guessedWrongLetters.length; j++) {
+			if(j > 0){
+				// don't need a comma before the first one
+				html2 += ", ";
+			}
+			html2 += guessedWrongLetters[j];	
+		}
+		document.querySelector('#guessedWrongLetters').innerHTML = html2;
+	},
+
+	gameOver : function(result){
+		console.log("gameOver: "+result);
+		waitingForAGuess = false;
+	}
+};
+
+/*
 var bodyParts = ["head", "torso", "right arm", "left arm", "right leg", "left leg"];
 var hangman = [false, false, false, false, false, false];
+
 
 // keep track of incorrect letters guessed
 var guessedWrongLetters = [];
@@ -44,6 +162,7 @@ var currentAnswer = "";
 
 // stop the game from registering key presses at some times
 var waitingForAGuess = true;
+
 
 function gameInit(){
 	document.addEventListener('keydown', function(event) {
@@ -167,4 +286,35 @@ function gameOver(result){
 	waitingForAGuess = false;
 }
 
-gameInit();
+*/
+// all possible words to guess
+var gameOfThronesDictionary = [
+	"Jaime",
+	"Cersei",
+	"Tyrion",
+	"Blackfish",
+	"Sansa",
+	"Arya",
+	"Summer",
+	"Winter",
+	"Others",
+	"Dothraki",
+	"Valyrian",
+	"Targaryen",
+	"Direwolf",
+	"Lannister",
+	"Riverrun",
+	"Winterfell",
+	"Dreadfort",
+	"Mereen",
+	"Sunspear",
+	"Clegane",
+	"Dragonfire",
+	"Stoneheart",
+	"Hodor",
+	"Coldhands",
+	"Mormont",
+	"Longclaw"
+];
+
+hangmanGame.gameInit(gameOfThronesDictionary);
